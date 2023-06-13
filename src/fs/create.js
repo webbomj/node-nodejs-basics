874db68dbf59eff44aad71f26c10aa2b3ced91b2
webbomj <nethack@mail.ru>
2023-06-13 20:53:29 +0300

    feat: add fs:create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs, { constants } from "node:fs/promises";
+import url from "node:url";
+import path from "node:path";
+
 const create = async () => {
-    // Write your code here 
+  const filename = url.fileURLToPath(import.meta.url);
+  const dirname = path.dirname(filename);
+
+  const newFileName = dirname + "\\files" + "\\fresh.txt";
+
+  let hasFile = false;
+  try {
+    await fs.access(newFileName, constants.W_OK);
+    hasFile = true;
+  } catch (e) {}
+
+  if (hasFile) {
+    throw new Error("FS operation failed");
+  }
+
+  await fs.writeFile(newFileName, "I am fresh and young");
 };
 
 await create();
