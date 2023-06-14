aec57c54709e9f41fec6f162f11b34f170c2fd49
webbomj <nethack@mail.ru>
2023-06-14 16:37:29 +0300

    feat: add zip tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import fs from "node:fs";
+import url from "node:url";
+import path from "node:path";
+import zlib from "node:zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const filename = url.fileURLToPath(import.meta.url);
+  const dirname = path.dirname(filename);
+
+  const fileToCompressPath = path.join(dirname, "files", "fileToCompress.txt");
+  const fileCompressedPath = path.join(dirname, "files", "archive.gz");
+
+  const writableStream = fs.createWriteStream(fileToCompressPath);
+  const readbleStream = fs.createReadStream(fileCompressedPath);
+
+  const gzipPipe = zlib.createUnzip();
+
+  readbleStream.pipe(gzipPipe).pipe(writableStream);
 };
 
 await decompress();
