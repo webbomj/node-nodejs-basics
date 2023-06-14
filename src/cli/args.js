9f6cb5837bdd4703c50cc91ecdf4889fcaf7cee4
webbomj <nethack@mail.ru>
2023-06-14 13:40:05 +0300

    feat: add cli:args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
+import process from "node:process";
+
 const parseArgs = () => {
-    // Write your code here 
+  const [, , ...args] = process.argv;
+  const newArgsArr = [];
+  for (let index = 0; index < args.length; index += 2) {
+    newArgsArr.push([args[index].replace("--", ""), args[index + 1]]);
+  }
+  const newArgsArrMap = newArgsArr.map((arr) => `${arr[0]} is ${arr[1]}`);
+  process.stdout.write(newArgsArrMap.join(", "));
 };
 
 parseArgs();
