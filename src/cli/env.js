a661308df884c285e2b777b4c99308646ced9f2d
webbomj <nethack@mail.ru>
2023-06-14 13:22:22 +0300

    feat: add cli:env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
+import process from "node:process";
+
 const parseEnv = () => {
-    // Write your code here 
+  const allENV = process.env;
+  const ENVKeys = Object.entries(allENV);
+  const RSSENVKEYS = ENVKeys.filter((key) => /RSS_\w*/gm.test(key[0])).map(
+    (key) => `${key[0]}=${key[1]}`
+  );
+  process.stdout.write(RSSENVKEYS.join("; "));
 };
 
 parseEnv();
