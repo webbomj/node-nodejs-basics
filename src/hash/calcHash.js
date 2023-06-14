2533d0d74bc8a39bee98de63eb5432b36fb0dee1
webbomj <nethack@mail.ru>
2023-06-14 14:57:21 +0300

    feat: add hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import crypto from "node:crypto";
+import url from "node:url";
+import path from "node:path";
+import fs from "node:fs/promises";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filename = url.fileURLToPath(import.meta.url);
+  const dirname = path.dirname(filename);
+
+  const fileToCalculatePath = path.join(
+    dirname,
+    "files",
+    "fileToCalculateHashFor.txt"
+  );
+
+  const fileText = await fs.readFile(fileToCalculatePath);
+
+  const result = crypto
+    .createHash("sha256")
+    .update(fileText.toString())
+    .digest("hex");
+  console.log(result);
 };
 
 await calculateHash();
