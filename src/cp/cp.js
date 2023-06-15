d4bcec55eba3436f828876557f8a4785a4b23458
webbomj <nethack@mail.ru>
2023-06-15 14:13:50 +0300

    feat: add cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import cp from "node:child_process";
+import path from "node:path";
+import url from "node:url";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const filname = url.fileURLToPath(import.meta.url);
+  const dirname = path.dirname(filname);
+  const scriptPath = path.join(dirname, "files", "script.js");
+
+  const child = cp.fork(scriptPath, args);
+  child.send(args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 3, 2]);
